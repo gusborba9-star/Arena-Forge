4c3b658ed909a878b9da3e337530535ed6bc88e6
gusborba9-star <gusborba9@gmail.com>
2026-09-15 22:02:18 -0300

    refactor(content): make ContentCatalog load scalable content

--- game/scripts/data/content_catalog.gd
+++ game/scripts/data/content_catalog.gd
@@ -1,8 +1,50 @@
 class_name ContentCatalog
 extends RefCounted
-var heroes := {}
-var cards := {}
-var arenas := {}
-func register_hero(value: Dictionary) -> void: heroes[value.id] = value
-func register_card(value: Dictionary) -> void: cards[value.id] = value
-func register_arena(value: Dictionary) -> void: arenas[value.id] = value
+
+var heroes: Dictionary = {}
+var cards: Dictionary = {}
+var arenas: Dictionary = {}
+var masteries: Dictionary = {}
+var forges: Dictionary = {}
+
+func register_hero(value: Dictionary) -> void:
+    var id := str(value.get("id", ""))
+    if id != "":
+        heroes[id] = value.duplicate(true)
+
+func register_card(value: Dictionary) -> void:
+    var id := str(value.get("id", ""))
+    if id != "":
+        cards[id] = value.duplicate(true)
+
+func register_arena(value: Dictionary) -> void:
+    var id := str(value.get("id", ""))
+    if id != "":
+        arenas[id] = value.duplicate(true)
+
+func register_mastery(value: Dictionary) -> void:
+    var id := str(value.get("id", ""))
+    if id != "":
+        masteries[id] = value.duplicate(true)
+
+func register_forge(value: Dictionary) -> void:
+    var id := str(value.get("id", ""))
+    if id != "":
+        forges[id] = value.duplicate(true)
+
+func load_initial_content() -> void:
+    for hero in HeroDefinitions.initial():
+        register_hero(hero)
+    for card in CardDefinitions.initial():
+        register_card(card)
+    for arena in ArenaDefinitions.initial():
+        register_arena(arena)
+
+func card_count() -> int:
+    return cards.size()
+
+func hero_count() -> int:
+    return heroes.size()
+
+func arena_count() -> int:
+    return arenas.size()
