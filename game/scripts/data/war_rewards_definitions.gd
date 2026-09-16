69d9029fa8864d6a18f25d67cf78302fbf43717a
gusborba9-star <gusborba9@gmail.com>
2026-09-15 22:23:59 -0300

    feat(social): add War rewards contract

---
+++ game/scripts/data/war_rewards_definitions.gd
@@ -0,0 +1,13 @@
+class_name WarRewardsDefinitions
+extends RefCounted
+
+static func create(data: Dictionary = {}) -> Dictionary:
+    return {
+        "id": str(data.get("id", "")),
+        "placement_rewards": data.get("placement_rewards", {}).duplicate(true),
+        "participation_rewards": data.get("participation_rewards", {}).duplicate(true),
+        "individual_contribution_rewards": data.get("individual_contribution_rewards", {}).duplicate(true),
+        "objective_rewards": data.get("objective_rewards", {}).duplicate(true),
+        "forge_rewards": data.get("forge_rewards", {}).duplicate(true),
+        "idempotency_key_policy": data.get("idempotency_key_policy", "war_id:player_id:reward_id")
+    }
