9b9ba295b5ed874b0bc871c008999ac808a1ae78
gusborba9-star <gusborba9@gmail.com>
2026-09-15 22:01:09 -0300

    feat(content): add future Forge specialization contract

---
+++ game/scripts/data/forge_definition.gd
@@ -0,0 +1,17 @@
+class_name ForgeDefinition
+extends RefCounted
+
+var id := ""
+var target_type := ""
+var target_id := ""
+var options: Array[Dictionary] = []
+var normalized_mode_policy: Dictionary = {}
+
+static func from_dict(value: Dictionary) -> ForgeDefinition:
+    var result := ForgeDefinition.new()
+    result.id = str(value.get("id", ""))
+    result.target_type = str(value.get("target_type", ""))
+    result.target_id = str(value.get("target_id", ""))
+    result.options = value.get("options", []).duplicate(true)
+    result.normalized_mode_policy = value.get("normalized_mode_policy", {}).duplicate(true)
+    return result
