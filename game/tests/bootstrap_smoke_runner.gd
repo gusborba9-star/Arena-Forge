20df54ba6aa56c605835bc868b39f6202600858d
gusborba9-star <gusborba9@gmail.com>
2026-09-17 20:10:57 -0300

    test(ci): standardize bootstrap runner exit contract

--- game/tests/bootstrap_smoke_runner.gd
+++ game/tests/bootstrap_smoke_runner.gd
@@ -1,18 +1,16 @@
 extends SceneTree
+const RunnerExit = preload("res://tests/support/runner_exit.gd")
 
 func _init() -> void:
     var scene := load("res://scenes/main.tscn") as PackedScene
     if scene == null:
-        push_error("BOOTSTRAP FAILURE: main scene could not be loaded")
-        quit(1)
+        RunnerExit.failure(self, ["BOOTSTRAP FAILURE: main scene could not be loaded"])
         return
     var instance := scene.instantiate()
     if instance == null:
-        push_error("BOOTSTRAP FAILURE: main scene could not be instantiated")
-        quit(1)
+        RunnerExit.failure(self, ["BOOTSTRAP FAILURE: main scene could not be instantiated"])
         return
     root.add_child(instance)
     await process_frame
-    print("ARENA_FORGE_BOOTSTRAP_SMOKE_OK main_scene=instantiated")
     instance.queue_free()
-    quit(0)
+    RunnerExit.success(self, "ARENA_FORGE_BOOTSTRAP_SMOKE_OK main_scene=instantiated")
